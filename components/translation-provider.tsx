1df72c68d8b40c569050e830da3c24578196baeb
moadzahafi98-hue <moadzahafi98@gmail.com>
2025-11-06 14:30:25 +0100

    Fix i18n initialization to render localized pages

--- components/translation-provider.tsx
+++ components/translation-provider.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode, useEffect, useState } from "react";
 import { I18nextProvider } from "react-i18next";
-import { getClientI18n } from "@/lib/i18n";
+import { createBrowserI18n } from "@/lib/i18n";
 import { Language } from "@/lib/i18n-config";
 
 interface Props {
@@ -12,22 +12,14 @@ interface Props {
 }
 
 export function TranslationProvider({ locale, resources, children }: Props) {
-  const [instance, setInstance] = useState<any>(null);
+  const [instance] = useState(() => createBrowserI18n(locale, resources));
 
   useEffect(() => {
-    getClientI18n(locale).then((i18nInstance) => {
-      Object.entries(resources).forEach(([ns, resource]) => {
-        if (!i18nInstance.hasResourceBundle(locale, ns)) {
-          i18nInstance.addResourceBundle(locale, ns, resource, true, true);
-        }
-      });
-      setInstance(i18nInstance);
+    Object.entries(resources).forEach(([ns, resource]) => {
+      instance.addResourceBundle(locale, ns, resource, true, true);
     });
-  }, [locale, resources]);
-
-  if (!instance) {
-    return null;
-  }
+    instance.changeLanguage(locale);
+  }, [instance, locale, resources]);
 
   return <I18nextProvider i18n={instance}>{children}</I18nextProvider>;
 }
